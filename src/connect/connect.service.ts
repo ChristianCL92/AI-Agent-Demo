e1a263be061cf1621c407679b0dbc40554ebd442
Christian <chrisdev92@outlook.com>
2025-04-16 14:31:21 +0200

    Implement Connect module with controller, and service, and DTOs

---
+++ src/connect/connect.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@nestjs/common';
+import Anthropic from '@anthropic-ai/sdk';
+import { CreateConnectDto } from './dto/create-connect.dto';
+import { UpdateConnectDto } from './dto/update-connect.dto';
+
+@Injectable()
+export class ConnectService {
+  private readonly anthropic: Anthropic;
+  
+  constructor () {
+    this.anthropic = new Anthropic({
+      apiKey:process.env.ANTHROPICS_API_KEY,
+    });
+  }
+
+  async message() {
+   try {
+    const msg = await this.anthropic.messages.create({
+      model: "claude-3-7-sonnet-20250219",
+      max_tokens: 1000,
+      temperature: 1,
+      system: "Respond only with short poems.",
+      messages: [
+              {
+              role: "user",
+              content: [
+            {
+              type: "text",
+              text: "Why is the ocean salty?"
+            }
+          ]
+        }
+    ]
+});
+    console.log(msg);
+    return {
+      poem: (msg.content[0] as { text: string }).text,
+      usage: msg.usage
+    }
+   } catch (error) {
+    throw new Error(`Could not perform Claude response ${error.message}`);
+   } 
+  
+}
+  create(createConnectDto: CreateConnectDto) {
+    return 'This action adds a new connect';
+  }
+
+  findAll() {
+    return `This action returns all connect`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} connect`;
+  }
+
+  update(id: number, updateConnectDto: UpdateConnectDto) {
+    return `This action updates a #${id} connect`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} connect`;
+  }
+}
