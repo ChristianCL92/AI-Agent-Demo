d5700a7474635ede05b3720fc2169e3f6b9d2c4b
Christian <chrisdev92@outlook.com>
2025-04-16 14:31:57 +0200

    Add NestJS config and mapped types dependencies; update AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConnectModule } from './connect/connect.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConnectModule,
+    ConfigModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
