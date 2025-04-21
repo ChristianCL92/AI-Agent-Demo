7ea800536c6dc16a16a34a19ad215dfe80cfdd75
Christian <chrisdev92@outlook.com>
2025-04-21 20:20:43 +0200

    make configModule global and include cliModule in the appModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConnectModule } from './connect/connect.module';
 import { ConfigModule } from '@nestjs/config';
+import { CliModule } from './cli/cli.module';
 
 @Module({
   imports: [
     ConnectModule,
-    ConfigModule.forRoot()
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CliModule,
   ],
   controllers: [AppController],
   providers: [AppService],
